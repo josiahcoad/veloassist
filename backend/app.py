27a2f9a3bf545db79a70909f0c6ccc573691d9c8
Josiah Coad <josiahcoad@Josiahs-MacBook-Air.local>
2020-02-11 17:27:54 -0600

    moved veoride/geometry functions to own file

--- backend/app.py
+++ backend/app.py
@@ -1,79 +1,17 @@
 from slackclient import SlackClient
 from flask import Flask, jsonify, request
 import numpy as np
-from scipy.spatial.distance import cdist
-import math
 from os import environ
 import json
 import requests
 from . import db
+from . import veoride as vr
 
 app = Flask(__name__)
 
 slack_token = environ.get('SLACK_API_KEY')
 assert slack_token is not None, 'Must supply a SLACK_API_KEY.'
 sc = SlackClient(slack_token)
-
-
-# Distance between two lat/lng points in meters
-def haversine(coord1, coord2):
-    R = 6372800  # Earth radius in meters
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
-
-    phi1, phi2 = math.radians(lat1), math.radians(lat2)
-    dphi = math.radians(lat2 - lat1)
-    dlambda = math.radians(lon2 - lon1)
-
-    a = math.sin(dphi/2)**2 + \
-        math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
-
-    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-
-# JSON Serialize Numpy
-class NpEncoder(json.JSONEncoder):
-    # pylint: disable=method-hidden
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NpEncoder, self).default(obj)
-
-
-def np_dumps(data):
-    return json.loads(json.dumps(data, cls=NpEncoder))
-
-
-# Core functionality
-def tag_bikes(stations, buffers, bikes):
-    buffers = np.array(buffers).reshape(-1, 1)
-    dists = cdist(stations, bikes, haversine)
-    in_buffer = dists <= buffers
-    masked = np.where(in_buffer, dists, np.Inf)
-    outliers = ~np.any(in_buffer, axis=0)
-    closest_stations = np.argmin(masked, axis=0)
-    bike_tags = np.where(outliers, -1, closest_stations)
-    return bike_tags
-
-
-def get_station_counts(bike_tags):
-    count = np.zeros(max(bike_tags)+1).astype(np.int64)
-    for x in bike_tags:
-        if x != -1:  # -1 is the tag for an outlier
-            count[x] += 1
-    return count
-
-
-def get_full_stations(stations, buffers, bikes, thresholds):
-    bike_tags = tag_bikes(stations, buffers, bikes)
-    station_counts = get_station_counts(bike_tags)
-    return station_counts >= thresholds
-
 
 # API Endpoints
 @app.route('/stations', methods=['GET', 'POST'])
@@ -124,15 +62,15 @@ def bike_tags():
     buffers = [station['radius'] for station in stations]
     centers = [(s['lat'], s['lng']) for s in stations]
     # compute
-    bike_tags = tag_bikes(centers, buffers, bikes)
-    return jsonify({'data': np_dumps(bike_tags)}), 200
+    bike_tags = vr.tag_bikes(centers, buffers, bikes)
+    return jsonify({'data': vr.np_dumps(bike_tags)}), 200
 
 
 @app.route('/station_counts', methods=['POST'])
 def station_counts():
     data = request.json
-    station_counts = get_station_counts(data)
-    return jsonify({'data': np_dumps(station_counts)}), 200
+    station_counts = vr.get_station_counts(data)
+    return jsonify({'data': vr.np_dumps(station_counts)}), 200
 
 
 @app.route('/slack_message', methods=['POST'])
