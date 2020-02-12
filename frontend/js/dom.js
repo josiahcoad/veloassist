babc36ec39f3bcd486b1f88e04aac1756b388a4f
allysonmking <allysonmking@tamu.edu>
2020-02-11 18:19:54 -0600

    part 2 of collapsible

--- frontend/js/dom.js
+++ frontend/js/dom.js
@@ -2,9 +2,26 @@ const addListItem = station => {
   const pctFull = Math.round((station.count / station.capacity) * 100);
   const exclaim = pctFull >= 100 ? '!!' : '';
   const text = `Station at ${pctFull}% capacity${exclaim} (${station.count}/${station.capacity})`;
-  const stationList = document.getElementById('station-list');
-  const newItem = document.createElement('li');
-  newItem.className = 'station-list-item';
-  newItem.innerText = text;
-  stationList.appendChild(newItem);
+  const newElement = 
+  `<button type="button" class="collapsible">Station ${station.id}</button>
+  <div class="content"> 
+    <p>${text}</p> 
+  </div>`;
+  const stationList = $('#station-list');
+  stationList.append(newElement);
 };
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
