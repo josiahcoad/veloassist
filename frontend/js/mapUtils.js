63332dc94d11ba33d9009ed9d3afec2e691feda8
Josiah Coad <josiahcoad@Josiahs-MacBook-Air.local>
2020-02-18 14:16:17 -0600

    removing some old code of Ally's

--- frontend/js/mapUtils.js
+++ frontend/js/mapUtils.js
@@ -113,20 +113,4 @@ const writeMap = async () => {
   showBikeMarkers(bikes);
   showStationMarkers(stations);
   stations.forEach(addListItem);
-
-  // show stations in panel
-  var acc = document.getElementsByClassName('accordion');
-  var i;
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener('click', function() {
-      this.classList.toggle('active');
-
-      var panel = this.nextElementSibling;
-      if (panel.style.display === 'block') {
-        panel.style.display = 'none';
-      } else {
-        panel.style.display = 'block';
-      }
-    });
-  }
 };
