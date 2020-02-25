251c5114a075b9c72bf5c4853398db13bd6b9b76
Josiah Coad <josiahcoad@Josiahs-MacBook-Air.local>
2020-02-25 13:58:32 -0600

    frontend moving code around

--- frontend/js/dom.js
+++ frontend/js/dom.js
@@ -1,1 +1,27 @@
+const addListItem = station => {
+  const pctFull = Math.round(station.fill * 100);
+  const newElement = `
+    <div class="accordion-wrap">
+      <button class="accordion" onclick="centerMap(${station.lat}, ${station.lng})">
+        <span class="right">${pctFull}% (${station.occupancy}/${station.capacity})</span>
+        <span class="left">Station ${station.id}</span>
+      </button>
+      <div class="panel">
+        <p class="center">Id: ${station.id} </p>
+        </br>
+        <p class="center">Type: ${station.Type} </p>
+      </div>
+      <hr class="nospace" />
+    </div>`;
+  $('.station-list').append(newElement);
+};
 
+const addAccordianEffect = () => {
+  $('.accordion-wrap').click(function() {
+    $('.panel').slideUp();
+    $(this)
+      .find('.panel')
+      .first()
+      .slideToggle();
+  });
+};
--- frontend/js/mapUtils.js
+++ frontend/js/mapUtils.js
@@ -1,24 +1,6 @@
 const makeMarkerIcon = color =>
   `https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|${color}|000000`;
 
-const colorArray = [
-  '05668D',
-  '427AA1',
-  '679436',
-  'A5BE00',
-  '4E598C',
-  'FCAF58',
-  'FF8C42',
-  'FFAE03',
-  'E67F0D',
-  'FE4E00',
-  'E9190F',
-  'FF0F80',
-  '008BF8',
-  '04E762',
-  'DC0073',
-];
-
 const makeStationCircle = station =>
   new google.maps.Circle({
     editable: true,
@@ -70,7 +52,7 @@ const addCircleInfo = (circle, content) => {
 const makeBikeMarker = bike => {
   const lat = bike.location.lat;
   const lng = bike.location.lng;
-  const color = bike.station ? colorArray[4 % colorArray.length] : 'ffffff';
+  const color = bike.station ? '6cf5a7' : 'ffffff';
   return new google.maps.Marker({
     position: { lat, lng },
     map,
@@ -90,34 +72,6 @@ const showBikeMarkers = bikes => {
       'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
     gridSize: 30,
     minimumClusterSize: 10,
-  });
-};
-
-const addListItem = station => {
-  const pctFull = Math.round(station.fill * 100);
-  const newElement = `
-  <div class="accordion-wrap">
-    <button class="accordion" onclick="centerMap(${station.lat}, ${station.lng})">
-      <span class="right">${pctFull}% (${station.occupancy}/${station.capacity})</span>
-      <span class="left">Station ${station.id}</span>
-    </button>
-    <div class="panel">
-      <p class="center">Id: ${station.id} </p>
-      </br>
-      <p class="center">Type: ${station.Type} </p>
-    </div>
-    <hr class="nospace" />
-  </div>`;
-  $('.station-list').append(newElement);
-};
-
-const addAccordianEffect = () => {
-  $('.accordion-wrap').click(function() {
-    $('.panel').slideUp();
-    $(this)
-      .find('.panel')
-      .first()
-      .slideToggle();
   });
 };
 
